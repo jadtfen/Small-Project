4fc13362a2ebbc1b572293feb52c66d35ae95e34
ckrick04 <ckr20004@gmail.com>
2024-05-29 20:37:09 -0400

    Renamed contactpage.html to make it the base page of the website

--- js/login.js
+++ js/login.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
             }),
             success: function(response) {
                 $('#loginForm')[0].reset();
-                window.location.assign("contactpage.html");
+                window.location.assign("index.html");
             },
             error: function(xhr) {
                 $('.error-text').html(xhr.responseJSON.message);
